ce0e2c5067371081adc711a3d91a049cda5a0fbd
Sheikh Islam <143849374+SheikhIslamD@users.noreply.github.com>
2024-04-01 17:33:29 -0400

    new camera system testing

--- Assets/Scripts/CameraControls.cs
+++ Assets/Scripts/CameraControls.cs
@@ -3,23 +3,35 @@ using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.InputSystem;
 
 public class CameraControls : MonoBehaviour
 {
     public GameObject followTransform;
     public Vector2 _look;
     public float rotationPower = 0.5f;
+    public float horizontal;
+    public float vertical;
+
+    private PlayerInput playerInput;
+    private InputAction lookAction;
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        lookAction = playerInput.actions["Look"];
     }
 
     // Update is called once per frame
     void Update()
     {
-        //not sure if this will work with gamepad :/
+        horizontal = Input.GetAxisRaw("Horizontal");
+		vertical = Input.GetAxisRaw("Vertical");
+
+        transform.rotation = Quaternion.Euler(0.0f, horizontal, 0.0f);
+/*        //not sure if this will work with gamepad :/
         _look.x += Input.GetAxis("Mouse X");
         _look.y += Input.GetAxis("Mouse Y");
 
@@ -44,6 +56,6 @@ public class CameraControls : MonoBehaviour
         //change player to rotate with mouse input too
         transform.rotation = Quaternion.Euler(0, followTransform.transform.rotation.eulerAngles.y, 0);
         //reset y rotation of look transform
-        followTransform.transform.localEulerAngles = new Vector3(angles.x, 0, 0);
+        followTransform.transform.localEulerAngles = new Vector3(angles.x, 0, 0);*/
     }
 }
