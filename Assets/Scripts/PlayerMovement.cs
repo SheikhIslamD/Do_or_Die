ea2acfd55c5d0194a359a2d25a91202765ca0cc9
Paul-Griffin-1517-1 <116929197+Paul-Griffin-1517-1@users.noreply.github.com>
2024-04-05 00:54:05 -0400

    Player animations generally functional

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
 	public int health = 3;
 	public CharacterController controller;
 	public Transform cam;
+	public Animator animator; 
 
 	public float speed = 7f;
 	public float turnSmooth = 0.1f;
@@ -39,6 +40,9 @@ public class PlayerMovement : MonoBehaviour
 		if (Input.GetButtonDown("Jump") && controller.isGrounded)
 		{
 			velocity = Mathf.Sqrt(jump * -2f * gravity);
+			animator.SetBool("is_running", false);
+			animator.SetBool("is_idle", false);
+			animator.SetTrigger("jump");
 		}
 
 		if (direction.magnitude >= 0.1f)
@@ -49,6 +53,27 @@ public class PlayerMovement : MonoBehaviour
 
 			Vector3 move = Quaternion.Euler(0f, orient, 0f) * Vector3.forward;
 			controller.Move(move.normalized * speed * Time.deltaTime);
+
+			if (controller.isGrounded)
+			{
+				animator.SetBool("is_running", true);
+				animator.SetBool("is_idle", false);
+			}
+			// TODO: In the appropriate script, call:   animator.SetTrigger("throw");  when the player throws the die. 
+
+		}
+		else
+		{
+			if (controller.isGrounded)
+			{
+				animator.SetBool("is_running", false);
+				animator.SetBool("is_idle", true);
+			}
+			else
+			{
+				animator.SetBool("is_running", false);
+				animator.SetBool("is_idle", false);
+			}
 		}
 
 		velocity += gravity * Time.deltaTime;
