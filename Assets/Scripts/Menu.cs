c1f5ff056d585574655a482f3b2d3fcc576b08a2
SirDante830 <dantec830@gmail.com>
2024-04-08 04:56:00 -0400

    Added Menus

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Platformer()
+    {
+        SceneManager.LoadScene("PlatformerPrototype");
+    }
+        
+    public void Maze()
+    {
+        SceneManager.LoadScene("MazePrototype");
+    }
+    public void Boss()
+    {
+        SceneManager.LoadScene("BossPrototype");
+    }
+}
