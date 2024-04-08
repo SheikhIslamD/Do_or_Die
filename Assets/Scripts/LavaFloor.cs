9b7b7bd4352abfc34540b0c23cfd0fb916a372b5
rickyje3 <rickyjelia@gmail.com>
2024-04-08 00:02:34 -0400

    Made stuff into prefabs so I can place it into prototype

--- Assets/Scripts/LavaFloor.cs
+++ Assets/Scripts/LavaFloor.cs
@@ -4,27 +4,16 @@ using UnityEngine;
 
 public class LavaFloor : MonoBehaviour
 {
-	public GameObject player;
-	public PlayerMovement playerScript;
-	public Transform respawn;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	PlayerMovement playerScript;
 
 	// This function is called when the collider on this GameObject collides with another collider
-	private void OnTriggerEnter(Collider other)
+	private void OnCollisionEnter(Collision other)
 	{
 		// Check if the collider we collided with has the Zone tag
-		if (other.CompareTag("Player"))
+		if (other.gameObject.CompareTag("Player"))
 		{
-			player.SetActive(false);
-			other.transform.position = respawn.position;
-			player.SetActive(true);
-			Debug.Log("PLayer stepped in lava");
 			playerScript.DamageHealth();
+			Debug.Log("PLayer stepped in lava");
 		}
 	}
 
