fc9500dd4f62ae83651353eee769c5f3e94f687c
LanaTherrien03 <la576625@ucf.edu>
2024-04-10 14:41:56 -0400

    Attempt at button programming x2
    Tried button programming again.

---
+++ Assets/LanaWorkshop/ButtonScriptDoor.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScriptDoor : MonoBehaviour
+//{
+    //public DoorScript doorScriptActivator;
+ 
+
+ //void OnCollisionEnter(Collision collision)
+//{
+    //if (collision.gameObject.CompareTag("Button"))
+    //{
+        //collidedObjects.Add(collision.gameObject);
+   // }
+
+//}
+      
+
+//}
---
+++ Assets/LanaWorkshop/DoorScriptActivator.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScriptActivator : MonoBehaviour
+//{
+    //private Animation animation; 
+   // public buttonScript ButtonScriptDoor;
+    //int targetCollidedObjects = 3;
+    
+   // List<GameObject> collidedObjects = new List<GameObject>();
+
+ //void update()
+//{
+    //if (collidedObjects == targetCollidedObjects)
+    //{
+        //animation.play("Open");
+   // }
+
+//}
+      
+
+//}
