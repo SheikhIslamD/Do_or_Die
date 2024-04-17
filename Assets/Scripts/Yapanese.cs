af4099fdc222bb012a4ddf048962393704cf3dca
SirDante830 <dantec830@gmail.com>
2024-04-17 17:18:10 -0400

    More work

--- Assets/Scripts/Yapanese.cs
+++ Assets/Scripts/Yapanese.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class Yapanese : MonoBehaviour
 {
+    public GameObject Tutorial_Dialogue;
+    private void Awake()
+    {
+        //talkText = GameObject.Find("Tutorial_Dialogue").GetComponent<PauseScript>();
+    }
     // This function is called when the collider on this GameObject collides with another collider
     private void OnTriggerEnter(Collider other)
     {
@@ -11,16 +18,20 @@ public class Yapanese : MonoBehaviour
         string triggerTag = gameObject.tag;
 
         // Check if the trigger collider has a specific tag
-        if (triggerTag == "HowToJump")
+        if (triggerTag == "HowToVent")
         {
             // Do something when the trigger collider's tag matches the specified tag
             Debug.Log("How to jump");
+            Tutorial_Dialogue.SetActive(true);
         }
 
         if (triggerTag == "HowToDice")
         {
             // Do something when the trigger collider's tag matches the specified tag
             Debug.Log("How to dice");
+            //Stache_Text_Vent.SetActive(false);
+            //Stache_Text_Jump.SetActive(true);
+
         }
 
         if (triggerTag == "HowToWall")
