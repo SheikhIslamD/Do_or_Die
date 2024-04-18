f645ef5e06b8f6d80a1141d675eabd0c5ab9167d
LanaTherrien03 <la576625@ucf.edu>
2024-04-18 14:17:19 -0400

    Added models to the maze and fixed some liners, walls and removed colliders.
    YAYAY

--- Assets/Scripts/LanaWorkshop/TeleportScript.cs
+++ Assets/Scripts/LanaWorkshop/TeleportScript.cs
@@ -3,21 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TeleportScript : MonoBehaviour
-
 {
-    public Transform teleportDestination;
+    public Vector3 teleportDestination; // Assign the destination coordinates in the Unity editor
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player")) // Assuming the player has the "Player" tag
         {
-            TeleportPlayer(other.gameObject);
+            TeleportPlayer(other.transform); // Pass the Transform component
         }
     }
 
-    private void TeleportPlayer(GameObject player)
+    private void TeleportPlayer(Transform playerTransform)
     {
-        player.transform.position = teleportDestination.position;
+        Debug.Log("Teleporting player to destination: " + teleportDestination);
+        playerTransform.position = teleportDestination; // Set player's position to teleportDestination
+        // You can optionally add some visual effects or animations here
+        Debug.Log("Player teleported successfully!");
     }
 }
-
